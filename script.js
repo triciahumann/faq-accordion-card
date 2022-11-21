a1f74023ecb20e5a512660561d5ed646ead2853d
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-21 14:31:44 -0800

    Tried an alternate eventListener

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const questionTitles = document.querySelector(".question-title");
 //the answer to each faq question
 const answer = document.querySelector(".answer");
 
-// When the question title is clicked
+//When the question title is clicked
 questionTitles.addEventListener("click", function () {
     console.log("hello");
     answer.classList.toggle("hide")
 })
 
+// questionTitles.forEach(question => {
+//     question.addEventListener('click', () => {
+//         question.nextElementSibling.classList.toggle("hide")
+//     })
+// })
