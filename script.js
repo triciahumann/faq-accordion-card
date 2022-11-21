acb16d345a59ba64504289ab3edf8e5c0bfe0976
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-21 15:15:43 -0800

    I don't even know - was trying to figure out how to get answers to appear

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-//title to each faq question 
-const questionTitles = document.querySelector(".question-title");
-//the answer to each faq question
-const answer = document.querySelector(".answer");
+// //title to each faq question 
+const questionTitles = document.querySelectorAll(".question-title");
+// //the answer to each faq question
+const answer = document.querySelector(".answer hide");
 
 //When the question title is clicked
-questionTitles.addEventListener("click", function () {
-    console.log("hello");
-    answer.classList.toggle("hide")
+// questionTitles.addEventListener("click", function () {
+//     console.log("hello");
+//     answer.classList.toggle("hide")
+// })
+
+questionTitles.forEach(question => {
+    question.addEventListener('click', () => {
+        console.log("hello");
+        //answer.classList.toggle("hide")
+        question.nextElementSibling.classList.toggle("hide")
+    })
 })
 
-// questionTitles.forEach(question => {
-//     question.addEventListener('click', () => {
-//         question.nextElementSibling.classList.toggle("hide")
-//     })
-// })
