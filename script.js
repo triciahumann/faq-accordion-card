b3edd3cc7e8c03c56fc0f5e2a1b785f92f3af4e3
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-21 15:23:05 -0800

    Comment to event listener

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ questionTitles.forEach(question => {
     question.addEventListener('click', () => {
         console.log("hello");
         //answer.classList.toggle("hide")
+        //nextElementSibling is not correct, it hides the arrow rather
+        // than the answer going in and out
         question.nextElementSibling.classList.toggle("hide")
     })
 })
