346ddd4a5c128d7b4b9eef724d52c2a017671fde
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-21 12:56:57 -0800

    Added hover color to question title

--- faq.js
+++ faq.js
@@ -1,1 +1,10 @@
-//
+//title to each faq question 
+const questionTitle = document.querySelector(".question-title");
+//the answer to each faq question
+const answer = document.querySelector(".answer");
+
+// When the question title is clicked
+questionTitle.addEventListener("click", function () {
+    e.preventDefault();
+    answer.ATTRIBUTE_NODE.classList.add("show");
+})
