7572b65de36ba3ea34247ffd0958a00bc71bc519
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-28 10:57:30 -0800

    Added click function and new updated styles

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 // //title to each faq question 
-const questionTitles = document.querySelectorAll(".question-title");
-// //the answer to each faq question
-const answer = document.querySelector(".answer hide");
+const questions = document.querySelectorAll(".question");
 
-//When the question title is clicked
+questions.forEach((question) => question.addEventListener
+('click', () => {
+    console.log("clicked");
+    //parentNode is the element above in HTML, 
+    //so the parentNode for .question is ".accordion-item"
+    if(question.parentNode.classList.contains("active")) {
+        question.parentNode.classList.toggle('active');
+    } else {
+        questions.forEach((question) => question.parentNode.classList.remove("active"))
+        question.parentNode.classList.add('active')
+    }
+})
+)
+
+
+
+// //When the question title is clicked
 // questionTitles.addEventListener("click", function () {
 //     console.log("hello");
 //     answer.classList.toggle("hide")
 // })
 
-questionTitles.forEach(question => {
-    question.addEventListener('click', () => {
-        console.log("hello");
-        //answer.classList.toggle("hide")
-        //nextElementSibling is not correct, it hides the arrow rather
-        // than the answer going in and out
-        question.nextElementSibling.classList.toggle("hide")
-    })
-})
+// questionTitles.forEach(question => {
+//     question.addEventListener('click', () => {
+//         console.log("hello");
+//         //answer.classList.toggle("hide")
+//         //nextElementSibling is not correct, it hides the arrow rather
+//         // than the answer going in and out
+//         question.nextElementSibling.classList.toggle("hide")
+//     })
+// })
 
